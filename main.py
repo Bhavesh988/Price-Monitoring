880b9e4d3523cffce58bc62e48049870d328ba88
Bhavesh988 <bhaveshparihyar988@gmail.com>
2020-06-28 13:30:25 +0530

    Added files for webscrapping

---
+++ main.py
@@ -0,0 +1,33 @@
+import urllib.request,urllib.parse,urllib.error
+from bs4 import BeautifulSoup
+from remove import *
+import time
+
+class product:
+    name: str = None
+    price: float = None
+    rate:str = None
+    def __init__(self,name=None, price=None,rate=None):
+        self.name = name
+        self.price = price
+        self.rate= rate
+
+_new = product()
+sender_email = input("Sender's email address:-")
+password =input("Enter password")
+receiver_email=input("Type your email address:-")
+
+n=int(input("Enter 1 for amazon , 2 for Flipcart:-"))
+if n==2:
+	urla=input('Enter url of product for Flipcart - ')
+	while True:
+		Flip().find_p(_new,urla,sender_email,receiver_email,password)
+		print("Price will be checked every 5 minutes")
+		time.sleep(300)
+elif n==1:
+	urlb=input('Enter url of product for Amazon - ')
+	while True:
+		Amz().find_p(_new,urlb,sender_email,receiver_email,password)
+		print("Price will be checked every 5 minutes")
+		time.sleep(300)
+
---
+++ remove.py
@@ -0,0 +1,50 @@
+import urllib.request,urllib.parse,urllib.error
+from bs4 import BeautifulSoup
+import ssl,smtplib
+
+ctx= ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verifymode = ssl.CERT_NONE
+
+smtp_server = "smtp.gmail.com"
+port = 587  # For starttls
+
+class Flip:
+	def find_p(self,_new,urla,sender_email,receiver_email,password):
+		req = urllib.request.Request(urla, headers={'User-Agent': 'Mozilla/5.0'})
+		html=urllib.request.urlopen(req,context=ctx).read()
+		soup = BeautifulSoup(html, 'html.parser')
+		name=soup.find('span', attrs={'class':"_35KyD6"}).text
+		price=float(soup.find('div', attrs={'class':"_1vC4OE _3qQ9m1"}).text[1:].replace(',',""))
+		rating=soup.find('div',attrs={'class':"hGSR34"}).text
+		check_price(_new,urla,sender_email,receiver_email,password,name,price,rating)
+		
+
+class Amz:
+	def find_p(self,_new,urla,sender_email,receiver_email,password):
+		req = urllib.request.Request(urla, headers={'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36'})
+		html=urllib.request.urlopen(req,context=ctx).read()
+		soup = BeautifulSoup(html, "lxml")
+		name=soup.find('span',attrs={'id':"productTitle"}).text
+		price=float(soup.find('span', attrs={'id':"priceblock_ourprice"}).text[2:].replace(',',""))
+		rating=0
+		check_price(_new,urla,sender_email,receiver_email,password,name,price,rating)
+
+def check_price(_new,urla,sender_email,receiver_email,password,name,price,rating):
+	if _new.price == None:
+		_new.name=name
+		_new.price=price
+		_new.rate=rating
+		print("Name of product:-{fname} \nPrice of product:-Rs {fprice} \n Rating of product:-{frate}".format(fname=_new.name,fprice=_new.price,frate=_new.rate))
+	elif _new.price>price:
+		_new.price=price
+		print("Name of product:-{fname} \nNew price of product:-Rs {fprice} \n Rating of product:-{frate}".format(fname=_new.name,fprice=_new.price,frate=_new.rate))
+		with smtplib.SMTP(smtp_server, port) as server:
+			server.ehlo()
+			server.starttls(context=ctx)
+			server.ehlo()
+			server.login(sender_email,password)
+			message=("Name is {fname} and the lower price is {cheap}".format(fname=_new.name,cheap=_new.price)).encode('utf-8')
+			server.sendmail(sender_email,receiver_email,message)
+	elif _new.price==price:
+		print("Price didn't reduce, you will receive an email if price is reduced")
